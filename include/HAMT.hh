7ed8652123022ce5c90cb40b670c75e3ad1bd1e0
Ian Kuehne <ikuehne617@gmail.com>
2019-08-17 20:38:10 -0700

    Stop leaking memory.

--- include/HAMT.hh
+++ include/HAMT.hh
@@ -8,21 +8,35 @@ class Leaf;
 
 static const std::uint64_t FIRST_6_BITS = (1 << 6) - 1;
 
+//////////////////////////////////////////////////////////////////////////////
+// Class prototypes.
+//
+
 // A pointer, except that the low bit is used to test whether it is a pointer
 // to a child node or a result.
 class HamtNodePointer {
 public:
     explicit HamtNodePointer(HamtNode *node)
-        : ptr(reinterpret_cast<std::uintptr_t>(node))
-    {}
+        : ptr(reinterpret_cast<std::uintptr_t>(node)) {}
 
-    explicit HamtNodePointer(void *data)
-        : ptr(reinterpret_cast<std::uintptr_t>(data) | 1)
-    {}
+    // Initialize the pointer 
+    explicit HamtNodePointer(Leaf *leaf)
+        : ptr(reinterpret_cast<std::uintptr_t>(leaf) | 1) {}
 
-    HamtNodePointer()
-        : ptr(0)
-    {}
+    // Initialize the pointer to NULL.
+    HamtNodePointer() : ptr(0) {}
+
+    HamtNodePointer(HamtNodePointer &&other): ptr(other.ptr) {
+        other.ptr = 0;
+    }
+
+    HamtNodePointer &operator=(HamtNodePointer &&other) {
+        ptr = other.ptr;
+        other.ptr = 0;
+        return *this;
+    }
+
+    ~HamtNodePointer();
 
     inline bool isChild() {
         return !(ptr & 1);
@@ -34,10 +48,6 @@ public:
 
     inline HamtNode *getChild() {
         return reinterpret_cast<HamtNode *>(ptr);
-    }
-
-    inline void setChild(HamtNode *child) {
-        ptr = reinterpret_cast<uintptr_t>(child);
     }
 
     inline Leaf *getLeaf() {
@@ -94,7 +104,7 @@ public:
         // subtract 1 from the count of set bits.
         std::uint64_t rest = map >> thisNodeKey;
         int idx = __builtin_popcountll((unsigned long long)rest) - 1;
-        HamtNodePointer next = children[idx];
+        HamtNodePointer &next = children[idx];
         if (next.isChild()) {
             assert(!next.isNull());
             return next.getChild()->lookup(hash >> 6);
@@ -104,71 +114,79 @@ public:
     }
 
     void insert(uint64_t hash, std::string *str) {
-        auto thisNodeKey = hash & FIRST_6_BITS;
-        bool hasChild = map & (1ULL << thisNodeKey);
-        std::uint64_t rest = map >> thisNodeKey;
-        // idx is the number of set bits *higher* than the bit we just
-        // checked (we'll adjust it in a second if this line counted the bit
-        // we just checked).
-        int idx = __builtin_popcountll((unsigned long long)rest);
+        auto nextNode = findChildForInsert(hash);
 
-        if (hasChild) {
-            // We counted the bit for our child--subtract that out.
-            idx -= 1;
-            HamtNodePointer &next = children[idx];
-            assert(!next.isNull());
-            if (next.isChild()) {
-                next.getChild()->insert(hash >> 6, str);
-            } else {
-                Leaf *oldLeaf = next.getLeaf();
-                std::uint64_t otherHash = oldLeaf->getHash();
-                if (otherHash == hash) {
-                    oldLeaf->getData().push_back(str);
-                } else {
-                    assert((otherHash & FIRST_6_BITS) == thisNodeKey);
-                    HamtNode *newNode = new HamtNode();
-                    next.setChild(newNode);
-                    newNode->insertLeaf(otherHash >> 6, oldLeaf);
-                    newNode->insert(hash >> 6, str);
-                }
-            }
-        } else {
-            // We need to add a new child. Set the bit in the map:
-            map |= (1ULL << thisNodeKey);
+        // Case 1: we created a new node at this level.
+        if (nextNode->isNull()) {
+            // We're done, so create a new leaf and put it in the node.
             Leaf *leaf = new Leaf(hash);
-            // Add the string to the new vector.
             leaf->getData().push_back(str);
-            // And stick it in its expected position.
-            children.insert(children.begin() + idx, HamtNodePointer(leaf));
+            *nextNode = HamtNodePointer(leaf);
+        // Case 2: we need to go another level down the HAMT.
+        } else if (nextNode->isChild()) {
+            return nextNode->getChild()->insert(hash >> 6, str);
+        // Case 3: we found a leaf already here. We need to replace it with a
+        // new node, and put both leaves in that node.
+        } else {
+            Leaf *oldLeaf = nextNode->getLeaf();
+            std::uint64_t otherHash = oldLeaf->getHash();
+            if (otherHash == hash) {
+                oldLeaf->getData().push_back(str);
+            } else {
+                assert((otherHash & FIRST_6_BITS) == (hash & FIRST_6_BITS));
+                HamtNode *newNode = new HamtNode();
+                *nextNode = HamtNodePointer(newNode);
+                newNode->insertLeaf(otherHash >> 6, oldLeaf);
+                newNode->insert(hash >> 6, str);
+            }
         }
     }
 
 private:
-    void insertLeaf(uint64_t hash, Leaf *leaf) {
+    inline std::uint64_t getIndex(std::uint64_t firstBits) {
+        std::uint64_t rest = map >> firstBits;
+        return __builtin_popcountll((unsigned long long)rest);
+    }
+
+    inline std::vector<HamtNodePointer>::iterator
+            findChildForInsert(uint64_t hash) {
         auto thisNodeKey = hash & FIRST_6_BITS;
-        bool hasChild = map & (1ULL << thisNodeKey);
-        std::uint64_t rest = map >> thisNodeKey;
-        int idx = __builtin_popcountll((unsigned long long)rest);
+        bool hasChild = (map & (1ULL << thisNodeKey)) != 0;
+        int idx = getIndex(thisNodeKey);
 
         if (hasChild) {
             idx -= 1;
-            HamtNodePointer &next = children[idx];
-            if (next.isChild()) {
-                return next.getChild()->insertLeaf(hash >> 6, leaf);
-            } else {
-                Leaf *oldLeaf = next.getLeaf();
-                std::uint64_t otherHash = oldLeaf->getHash();
-                HamtNode *newNode = new HamtNode();
-                next.setChild(newNode);
-                newNode->insertLeaf(otherHash >> 6, oldLeaf);
-                newNode->insertLeaf(hash >> 6, leaf);
-            }
+            return children.begin() + idx;
         } else {
             // We need to add a new child. Set the bit in the map:
             map |= (1ULL << thisNodeKey);
-            leaf->setHash(hash);
             // And stick it in its expected position.
-            children.insert(children.begin() + idx, HamtNodePointer(leaf));
+            children.insert(children.begin() + idx, HamtNodePointer());
+            return children.begin() + idx;
+        }
+    }
+
+    void insertLeaf(uint64_t hash, Leaf *leaf) {
+        auto nextNode = findChildForInsert(hash);
+
+        // Case 1: we created a new node at this level.
+        if (nextNode->isNull()) {
+            // We're done, so set the leaf's hash.
+            leaf->setHash(hash);
+            // Put the leaf in the new node.
+            *nextNode = HamtNodePointer(leaf);
+        // Case 2: we need to go another level down the HAMT.
+        } else if (nextNode->isChild()) {
+            return nextNode->getChild()->insertLeaf(hash >> 6, leaf);
+        // Case 3: we found a leaf already here. We need to replace it with a
+        // new node, and put both leaves in that node.
+        } else {
+            Leaf *oldLeaf = nextNode->getLeaf();
+            std::uint64_t otherHash = oldLeaf->getHash();
+            HamtNode *newNode = new HamtNode();
+            *nextNode = HamtNodePointer(newNode);
+            newNode->insertLeaf(otherHash >> 6, oldLeaf);
+            newNode->insertLeaf(hash >> 6, leaf);
         }
     }
     // The map goes low bits to high bits. We'll pretend it's 4 bits instead
@@ -188,6 +206,9 @@ private:
 class Hamt {
 public:
     Hamt() : root(new HamtNode()), hasher() {}
+    ~Hamt() {
+        delete root;
+    }
 
     void insert(std::string *str) {
         std::uint64_t hash = hasher(*str);
@@ -208,3 +229,13 @@ private:
     HamtNode *root;
     std::hash<std::string> hasher;
 };
+
+HamtNodePointer::~HamtNodePointer() {
+    if (isNull()) {
+        return;
+    } else if (isChild()) {
+        delete getChild();
+    } else {
+        delete getLeaf();
+    }
+}
--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <iostream>
 #include <random>
+#include <memory>
 #include <unordered_set>
 
 #include "HAMT.hh"
@@ -12,10 +13,10 @@ void die() {
 
 static auto generator = std::mt19937();
 
-std::string *random_string()
+std::unique_ptr<std::string>random_string()
 {
     int length = generator() % 256;
-    auto str = new std::string(length,0);
+    auto str = std::make_unique<std::string>(length,0);
 
     std::generate_n(str->begin(), length, generator);
 
@@ -24,42 +25,43 @@ std::string *random_string()
 
 int main(void) {
     std::unordered_set<std::string> setOfStringsToAdd;
-    std::vector<std::string *> stringsToAdd;
+    std::vector<std::unique_ptr<std::string>> stringsToAdd;
 
     for (int i = 0; i < 20000; ++i) {
         auto str = random_string();
-        stringsToAdd.push_back(str);
         setOfStringsToAdd.insert(*str);
+        stringsToAdd.push_back(std::move(str));
     }
 
-    std::vector<std::string*> stringsNotToAdd;
+    std::vector<std::unique_ptr<std::string>> stringsNotToAdd;
 
     for (int i = 0; i < 20000; ++i) {
         auto str = random_string();
         if (setOfStringsToAdd.find(*str) != setOfStringsToAdd.end()) {
             continue;
         }
-        stringsNotToAdd.push_back(str);
+
+        stringsNotToAdd.push_back(std::move(str));
     }
 
     Hamt hamt;
 
     int i = 0;
-    for (auto str: stringsToAdd) {
+    for (auto &str: stringsToAdd) {
         i += 1;
-        hamt.insert(str);
+        hamt.insert(str.get());
     }
 
     i = 0;
-    for (auto str: stringsToAdd) {
+    for (auto &str: stringsToAdd) {
         i += 1;
-        if (!hamt.lookup(str)) die();
+        if (!hamt.lookup(str.get())) die();
     }
 
     i = 0;
-    for (auto str: stringsNotToAdd) {
+    for (auto &str: stringsNotToAdd) {
         i += 1;
-        if (hamt.lookup(str)) die();
+        if (hamt.lookup(str.get())) die();
     }
 
     return 0;
